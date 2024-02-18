3eda3d4f6dc5d27e398946c6e6c784963f78ba88
nathanieluriri <95766173+nathanieluriri@users.noreply.github.com>
2024-02-18 15:00:00 +0100

    Initial commit

---
+++ src/personal/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+from operator import attrgetter
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+
+from blog.models import BlogPost
+from blog.views import get_blog_queryset
+
+BLOG_POST_PER_PAGE = 10
+
+def home_screen_view(request):
+
+    context = {}
+
+    query = ""
+    if request.GET:
+        query = request.GET('q', '')
+        context['query'] = str(query)
+        
+    blog_posts = sorted(get_blog_queryset(query), key=attrgetter('date_updated'), reverse=True)
+    
+
+    #Pagination
+    page = request.GET.get('page', 1)
+    blog_post_paginator = Paginator(blog_posts, BLOG_POST_PER_PAGE)
+
+    try:
+        blog_posts = blog_post_paginator.page(page)
+    except PageNotAnInteger:
+        blog_posts = blog_post_paginator.page(BLOG_POST_PER_PAGE)
+    except EmptyPage:
+        blog_posts = blog_post_paginator.page(blog_post_paginator.num_pages)
+
+    context['blog_posts'] = blog_posts
+
+    return render(request, "personal/home.html", context)
